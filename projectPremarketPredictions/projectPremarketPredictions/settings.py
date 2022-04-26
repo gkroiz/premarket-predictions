2d503992b5accc4ce668ebcd390a3045846a1481
Gerson Kroiz <gersonkroiz@gmail.com>
2022-04-25 20:09:50 -0400

    mysql connected

--- projectPremarketPredictions/projectPremarketPredictions/settings.py
+++ projectPremarketPredictions/projectPremarketPredictions/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'projectPremarketPredictions.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'premarket_predictions_db',
+        'USER': 'root',
+        'PASSWORD': 'rootpassword',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
